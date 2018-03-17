c2047e12fe826cb1184d56467b97891cf446d9f7
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-03-17 21:52:00 +0300

    Hotfix

--- src/main/kotlin/ru/ifmo/networks/common/extensions.kt
+++ src/main/kotlin/ru/ifmo/networks/common/extensions.kt
@@ -5,18 +5,24 @@ import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
+fun ServerResponse.BodyBuilder.withDefaulHeader(): ServerResponse.BodyBuilder =
+        accessControlAllowOrigin()
+
 fun ServerResponse.BodyBuilder.accessControlAllowOrigin(): ServerResponse.BodyBuilder =
         header("Access-Control-Allow-Origin", "*")
 
 fun ServerResponse.BodyBuilder.writeByteContent(content: ByteArray): Mono<ServerResponse> =
-        header("Accept-Ranges", "bytes")
+        accessControlAllowOrigin()
+                .header("Accept-Ranges", "bytes")
                 .header("Access-Control-Expose-Headers", "Content-Length")
                 .contentLength(content.size.toLong())
                 .body(BodyInserters.fromObject(content))
 
-fun ServerResponse.BodyBuilder.json(): ServerResponse.BodyBuilder = contentType(MediaType.APPLICATION_JSON_UTF8)
+fun ServerResponse.BodyBuilder.json(): ServerResponse.BodyBuilder =
+        accessControlAllowOrigin().contentType(MediaType.APPLICATION_JSON_UTF8)
 
-fun <T> ServerResponse.BodyBuilder.json(value: T): Mono<ServerResponse> = json().body(BodyInserters.fromObject(value))
+fun <T> ServerResponse.BodyBuilder.json(value: T): Mono<ServerResponse> =
+        accessControlAllowOrigin().json().body(BodyInserters.fromObject(value))
 
 fun <T> ServerResponse.BodyBuilder.jsonSuccess(value: T): Mono<ServerResponse> = json(Response(value))
 
