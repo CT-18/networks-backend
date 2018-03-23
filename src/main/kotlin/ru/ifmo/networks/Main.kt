0e14348cec705232ffe85e4ec134020db1b93005
Yakutov Dmitry <miti_95@mail.ru>
2018-03-23 21:46:13 +0300

    Usage added.

--- src/main/kotlin/ru/ifmo/networks/Main.kt
+++ src/main/kotlin/ru/ifmo/networks/Main.kt
@@ -5,9 +5,14 @@ import ru.ifmo.networks.master.MasterRunner
 import ru.ifmo.networks.slave.SlaveRunner
 
 fun main(args: Array<String>) {
-    when (args[0].toLowerCase()) {
+    when (args.getOrNull(0)?.toLowerCase()) {
         "master" -> MasterRunner.run(args)
         "slave" -> SlaveRunner.run(args)
         "balancer" -> BalancerRunner.run(args)
+        else -> showUsage()
     }
+}
+
+fun showUsage() {
+    System.err.println("args[0] must be in { master, slave, balancer }.")
 }
