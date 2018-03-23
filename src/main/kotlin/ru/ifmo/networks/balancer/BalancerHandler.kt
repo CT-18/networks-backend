f3727cf1645f7491ec9cff9cd8e67593d68f8315
Yakutov Dmitry <miti_95@mail.ru>
2018-03-23 21:46:34 +0300

    Balancer optimized.

--- src/main/kotlin/ru/ifmo/networks/balancer/BalancerHandler.kt
+++ src/main/kotlin/ru/ifmo/networks/balancer/BalancerHandler.kt
@@ -6,20 +6,19 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 import ru.ifmo.networks.common.jsonSuccess
 import ru.ifmo.networks.common.withDefaultHeader
-import java.io.InputStreamReader
 import java.util.*
+import kotlin.math.max
 
 @Component
 class BalancerHandler {
 
     companion object {
         var urls = listOf<String>()
+        private val rnd = Random()
     }
-
-    private val rnd = Random()
 
     fun getNodeUrl(serverRequest: ServerRequest): Mono<ServerResponse> =
             ServerResponse.ok()
                     .withDefaultHeader()
-                    .jsonSuccess(mapOf("result" to urls.shuffled().firstOrNull()))
+                    .jsonSuccess(mapOf("result" to urls.getOrNull(rnd.nextInt(max(1, urls.size)))))
 }
