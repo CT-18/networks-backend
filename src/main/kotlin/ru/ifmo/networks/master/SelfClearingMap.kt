3a1bdbdcdd96a60f6b35f82659e23bf6cca4d5ff
lapenok-aleksej <lapenok.aleksej@gmail.com>
2018-04-05 00:30:28 +0300

    add cache of streams

--- src/main/kotlin/ru/ifmo/networks/master/SelfClearingMap.kt
+++ src/main/kotlin/ru/ifmo/networks/master/SelfClearingMap.kt
@@ -1,7 +1,10 @@
 package ru.ifmo.networks.master
 
-import java.util.Timer
-import java.util.TimerTask
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
 
@@ -10,6 +13,10 @@ class SelfClearingMap {
     private val allStreams = ConcurrentHashMap<String, StreamBaseUrlAndFragment>()
 
     private val updateTimes = ConcurrentHashMap<String, Long>()
+
+    private val writers = ConcurrentHashMap<String, Process>()
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
 
     init {
         allStreams["petrovich"] = StreamBaseUrlAndFragment("http://10.8.0.3", "live.m3u8")
@@ -25,16 +32,25 @@ class SelfClearingMap {
     }
 
     fun update(name: String, baseUrlAndFragment: StreamBaseUrlAndFragment) {
-        allStreams.put(name, baseUrlAndFragment)
-        updateTimes.put(name, System.currentTimeMillis())
+        allStreams[name] = baseUrlAndFragment
+        updateTimes[name] = System.currentTimeMillis()
+        val dir = Paths.get("$CACHE_DIR$name")
+        if (!Files.exists(dir))
+            try {
+                Files.createDirectories(dir)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        writers[name] = ProcessBuilder("ffmpeg", "-i", "${baseUrlAndFragment.baseUrl}${baseUrlAndFragment.fragment}",
+                "-vcodec", "copy", "-acodec", "copy", "$CACHE_DIR$name/file_${System.currentTimeMillis()}.mp4").start()
     }
 
     fun getStream(name: String): StreamBaseUrlAndFragment? {
-        return allStreams[name];
+        return allStreams[name]
     }
 
     fun asList(): List<Pair<String, StreamBaseUrlAndFragment>> {
-        return allStreams.toList();
+        return allStreams.toList()
     }
 
     private fun selfClean() {
@@ -42,6 +58,7 @@ class SelfClearingMap {
             if (System.currentTimeMillis() - lastSeen > CLEANING_PERIOD) {
                 allStreams.remove(s)
                 updateTimes.remove(s)
+                writers.remove(s)?.destroy()
             }
         }
     }
@@ -50,6 +67,7 @@ class SelfClearingMap {
 
     companion object {
         private val CLEANING_PERIOD = TimeUnit.MINUTES.toMillis(10)
+        private const val CACHE_DIR = "cache/"
     }
 
 }
